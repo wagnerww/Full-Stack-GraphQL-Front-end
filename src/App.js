26d82f410dcade4f3b2c12fed6f24fc042ed3d32
wagner <wagnerricardonet@gmail.com>
2019-08-03 14:30:15 -0300

    chore:primeira query de testes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import Recipes from "./components/Recipes";
 
 function App() {
+  const client = new ApolloClient({
+    uri: "http://localhost:4444/graphql"
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Home</h1>
+        <Recipes />
+      </div>
+    </ApolloProvider>
   );
 }
 
---
+++ src/queries/index.js
@@ -0,0 +1,11 @@
+import { gql } from "apollo-boost";
+
+export const GET_ALL_RECIPES = gql`
+  query {
+    getAllRecipes {
+      name
+      category
+      description
+    }
+  }
+`;
