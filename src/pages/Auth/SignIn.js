a57b4f1b7d837aa77ad1715f7302b5f13ff7d8f2
wagner <wagnerricardonet@gmail.com>
2019-08-06 08:07:28 -0300

    chore:finalizando o login

--- src/pages/Auth/SignIn.js
+++ src/pages/Auth/SignIn.js
@@ -1,5 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation } from "react-apollo-hooks";
+import { SIGNIN_USER } from "../../queries";
+
+import Error from "../../components/Error";
+
+const INITIAL_STATE = {
+  username: "",
+  password: ""
+};
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  const [user, setNewUser] = useState(INITIAL_STATE);
+
+  const [SignUser, { error, loading, data }] = useMutation(SIGNIN_USER);
+
+  function clearState() {
+    setNewUser(INITIAL_STATE);
+  }
+
+  function validateForm() {
+    const { username, password } = user;
+    const isInvalid = !username || !password;
+
+    return isInvalid;
+  }
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setNewUser({ ...user, [name]: value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    SignUser({
+      variables: {
+        ...user
+      }
+    }).then(data => {
+      console.log("data", data);
+      clearState();
+    });
+  }
+
+  return (
+    <div className="App">
+      <h2 className="App">Sigin</h2>
+      <form action="" className="form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          onChange={handleChange}
+          value={user.username}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+          value={user.password}
+        />
+        <button
+          type="submit"
+          className="button-primary"
+          disabled={loading || validateForm()}
+        >
+          Submit
+        </button>
+        {error && <Error error={error} />}
+      </form>
+    </div>
+  );
 }
--- src/queries/index.js
+++ src/queries/index.js
@@ -10,6 +10,14 @@ export const GET_ALL_RECIPES = gql`
   }
 `;
 
+export const SIGNIN_USER = gql`
+  mutation($username: String!, $password: String!) {
+    signInUser(username: $username, password: $password) {
+      token
+    }
+  }
+`;
+
 export const SIGNUP_USER = gql`
   mutation($username: String!, $email: String!, $password: String!) {
     signupUser(username: $username, email: $email, password: $password) {
